8ca74db1c34b21e8469c774ebaedc7dedc4cb87a
nshebeko <nikolay.shebeko@gmail.com>
2019-10-13 07:24:26 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+  focus = 'family',
+  knowsProgramming = true,
+  config = {
+    family: 4
+  }
+) {
+    return knowsProgramming ? Math.ceil(800 / config[focus]) : Math.ceil(800 / config[focus] + 500 / config[focus]);
+};
